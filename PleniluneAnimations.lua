d5a8e126a01b4e1abddafcda9be6942772acc86b
4sigils <xdmakesmebad@gmail.com>
2026-09-22 00:56:10 -0400

    Update PleniluneAnimations.lua

--- PleniluneAnimations.lua
+++ PleniluneAnimations.lua
@@ -97,7 +97,7 @@ return {
     ["rbxassetid://79977678386966"] = { name = "Aerial",   range = 20, pbs = {0.4} },
     ["rbxassetid://18777237155"] = { name = "Chain Pull",   range = 15, pbs = {0.55} },
 
-    ["rbxassetid://18758324413"] =  { name = "1",   range = 20, pbs = {0.65} },
+    ["rbxassetid://18758324413"] =  { name = "1",   range = 20, pbs = {0.6} },
     ["rbxassetid://105250400900265"] = { name = "2",   range = 20, pbs = {0.55} },
     ["rbxassetid://18777324350"] = { name = "3",   range = 20, pbs = {0.4} },
     ["rbxassetid://76544691350588"] = { name = "4",   range = 20, pbs = {0.4} },
