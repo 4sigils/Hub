195c71b6f6957e5d95ac6160b137c973ab2954ba
4sigils <xdmakesmebad@gmail.com>
2026-09-21 20:56:09 -0400

    Update PleniluneAnimations.lua

--- PleniluneAnimations.lua
+++ PleniluneAnimations.lua
@@ -1,6 +1,19 @@
 return {
-    --["rbxassetid://"] = { name = "M1",   range = 15, pbs = {0.05} },
-    -- Catalyst
+    --[[
+    ["rbxassetid://1"] =  { name = "M1",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "M2",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "M3",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "M4",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "Critical",   range = 20, pbs = {0.55} },
+    ["rbxassetid://1"] = { name = "Aerial",   range = 20, pbs = {0.4} },
+
+    ["rbxassetid://1"] =  { name = "1",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "2",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "3",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "4",   range = 20, pbs = {0.4} },
+    ]]
+    
+    -- // Catalyst \\ --
     ["rbxassetid://76111197346761"] =  { name = "M1",   range = 20, pbs = {0.4} },
     ["rbxassetid://113692670920130"] = { name = "M2",   range = 20, pbs = {0.4} },
     ["rbxassetid://106421464937565"] = { name = "M3",   range = 20, pbs = {0.4} },
@@ -12,4 +25,19 @@ return {
     --["rbxassetid://18568979941"] = { name = "2",   range = 20, pbs = {0.5} },
     ["rbxassetid://18670018533"] = { name = "3",   range = 30, pbs = {0.45} },
     --["rbxassetid://18585088692"] = { name = "4",   range = 20, pbs = {0.45} },
+
+    -- // Elphael \\ --
+    -- Fist
+    ["rbxassetid://123272586030523"] =  { name = "M1",   range = 15, pbs = {0.4} },
+    ["rbxassetid://99575418719551"] = { name = "M2",   range = 15, pbs = {0.4} },
+    ["rbxassetid://71895146677345"] = { name = "M3",   range = 15, pbs = {0.4} },
+    ["rbxassetid://132412026004397"] = { name = "M4",   range = 15, pbs = {0.4} },
+    ["rbxassetid://74767361010589"] = { name = "Critical",   range = 20, pbs = {0.55} },
+    ["rbxassetid://79977678386966"] = { name = "Aerial",   range = 20, pbs = {0.4} },
+    ["rbxassetid://18563936434"] = { name = "4",   range = 15, pbs = {0.4} },
+    
+    ["rbxassetid://1"] =  { name = "1",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "2",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "3",   range = 20, pbs = {0.4} },
+    ["rbxassetid://1"] = { name = "4",   range = 20, pbs = {0.4} },
 }
