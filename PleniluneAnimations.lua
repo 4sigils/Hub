e86cde69bf9c84ca00b6981639e823b0a64936a5
4sigils <xdmakesmebad@gmail.com>
2026-09-21 20:07:41 -0400

    Update PleniluneAnimations.lua

--- PleniluneAnimations.lua
+++ PleniluneAnimations.lua
@@ -1,11 +1,11 @@
 return {
     --["rbxassetid://"] = { name = "M1",   range = 15, pbs = {0.05} },
     -- Catalyst
-    ["rbxassetid://76111197346761"] = { name = "M1",   range = 20, pbs = {0.2} },
-    ["rbxassetid://113692670920130"] = { name = "M2",   range = 20, pbs = {0.2} },
-    ["rbxassetid://106421464937565"] = { name = "M3",   range = 20, pbs = {0.2} },
-    ["rbxassetid://110220095647062"] = { name = "M4",   range = 20, pbs = {0.2} },
-    ["rbxassetid://111904150533513"] = { name = "Critical",   range = 20, pbs = {0.2} },
+    ["rbxassetid://76111197346761"] = { name = "M1",   range = 20, pbs = {0.45} },
+    ["rbxassetid://113692670920130"] = { name = "M2",   range = 20, pbs = {0.45} },
+    ["rbxassetid://106421464937565"] = { name = "M3",   range = 20, pbs = {0.45} },
+    ["rbxassetid://110220095647062"] = { name = "M4",   range = 20, pbs = {0.45} },
+    ["rbxassetid://111904150533513"] = { name = "Critical",   range = 20, pbs = {0.55} },
 
     
 }
